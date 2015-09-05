3545f6a97c43988491ddb538931ecdd5d4e8402c
Desmond McNamee <desmondmcnamee@gmail.com>
2015-09-05 14:27:26 +0100

    Started using apsdehal go-logger

--- httpLogger.go
+++ httpLogger.go
@@ -1,23 +1,30 @@
 package httpLogger
 
 import (
-	"log"
+	"fmt"
 	"net/http"
+	"os"
 	"time"
+
+	"github.com/apsdehal/go-logger"
 )
 
 func Logger(inner http.Handler, name string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
-
 		inner.ServeHTTP(w, r)
 
-		log.Printf(
-			"%s\t%s\t%s\t%s",
+		log, err := logger.New("httpLogger", 1, os.Stdout)
+		if err != nil {
+			panic(err) // Check for error
+		}
+
+		msg := fmt.Sprintf("New Request: %s\t%s\t%s\t%s",
 			r.Method,
 			r.RequestURI,
 			name,
-			time.Since(start),
-		)
+			time.Since(start))
+
+		log.Notice(msg)
 	})
 }
